21d86fecf278eec61b8159755d0e251014d1e0ae
Ido <ido.tsehori@gmail.com>
2023-03-22 11:58:59 +0200

    dettails

--- src/views/StayDetails.vue
+++ src/views/StayDetails.vue
@@ -1,17 +1,68 @@
 <template>
-  <main class="stay-review">
-    <section class="stay-info">
-      <h1>Name Something cool</h1>
-      <div>
-        <span>⭐4.9</span>
-        <span>2 reviews</span>
-        <span>Barcelona, Spain</span>
+  <main class="stay-details">
+    <!-- stay-review -->
+    <section class="stay-review flex column">
+      <h1>Great location in Barcelona</h1>
+      <section class="flex space-between">
+        <div class="flex">
+          <span>⭐4.9</span>
+          <span>2 reviews</span>
+          <span>🥇 Superhost</span>
+          <span>Barcelona, Spain</span>
+        </div>
+
+        <div class="flex">
+          <span>💿Share</span>
+          <span>♥Save</span>
+        </div>
+      </section>
+    </section>
+    <!-- Img-container -->
+    <section class="img-container">
+      <img class="main-img" src="../../imgs_test/ido_test.jpg" alt="img" />
+      <img class="img-1" src="../../imgs_test/ido_test.jpg" alt="img" />
+      <img class="img-2" src="../../imgs_test/ido_test.jpg" alt="img" />
+      <img class="img-3" src="../../imgs_test/ido_test.jpg" alt="img" />
+      <img class="img-4" src="../../imgs_test/ido_test.jpg" alt="img" />
+    </section>
+
+    <!-- Details-container -->
+    <section class="details-container">
+      <div class="stay-ferrites">
+        <div class="ferrites-header">
+          <div class="header-text">
+            <h3>Entire amazing views hosted by Marina E Mickael</h3>
+            <p>5 guests · 5 bedrooms · 10 beds · 3 baths</p>
+          </div>
+          <img src="../../imgs_test/ido_test.jpg" />
+        </div>
+        <div class="ferrites-main">
+          <div class="line">
+            <span>🚽</span>
+            <div class="text">
+              <h4>Dedicated workspace</h4>
+              <p>A private room with wifi that's well-suited for working.</p>
+            </div>
+          </div>
+          <div class="line">
+            <span>🚽</span>
+            <div class="text">
+              <h4>Dedicated workspace</h4>
+              <p>A private room with wifi that's well-suited for working.</p>
+            </div>
+          </div>
+
+          <div class="line">
+            <span>🚽</span>
+            <div class="text">
+              <h4>Dedicated workspace</h4>
+              <p>A private room with wifi that's well-suited for working.</p>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div>
-        <span>💿Share</span>
-        <span>♥Save</span>
-      </div>
+      <div class="sticky-container"></div>
     </section>
   </main>
 </template>
