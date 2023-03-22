c53b64e3efee58e76e216321da7494a922a4d71c
Omer <omerbnb@gmail.com>
2023-03-22 14:15:07 +0200

    more header design

---
+++ src/cmps/UserOptions.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="user-nav">
+        <div class="user-options">
+          <div>Login</div>
+          <div>Signup</div>
+          <span class="user-line"></span>
+          <div>Airgpt your home</div>
+        </div>
+      </div>
+</template>
+<script>
+export default {
+name: '',
+data() {
+return {
+
+}
+},
+methods: {
+
+},
+computed: {
+
+},
+created() {
+
+},
+components: {
+
+},
+}
+</script>
+
+<style>
+
+</style>
