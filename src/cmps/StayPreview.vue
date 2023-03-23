835020009aa3140a5c28c60fc989d5e2fb5e3692
Inon <inon.f4@gmail.com>
2023-03-23 22:01:01 +0200

    preview

--- src/cmps/StayPreview.vue
+++ src/cmps/StayPreview.vue
@@ -1,29 +1,31 @@
 <template>
   <section class="stay-card">
     <StayImgPreview :imgs="stay.imgUrls" />
-    <h1 class="stay-name flex">
-      {{ stay.name }}
-      <p class="rate flex">
-        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation"
-          focusable="false" style="
-                          display: block;
-                          height: 14px;
-                          width: 14px;
-                          margin-bottom: 1px;
-                          fill: currentcolor;
-                        ">
-          <path
-            d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z"
-            fill-rule="evenodd"></path>
-        </svg>
-        {{ stay.reviews[0].rate }} ({{ stay.reviews.length }})
+    <section class="info">
+      <p class="stay-name flex">
+        {{ stay.name }}
+        <span class="rate flex">
+          <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation"
+            focusable="false" style="
+                                  display: block;
+                                  height: 14px;
+                                  width: 14px;
+                                  margin-bottom: 1px;
+                                  fill: currentcolor;
+                                ">
+            <path
+              d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z"
+              fill-rule="evenodd"></path>
+          </svg>
+          {{ stay.reviews[0].rate }} ({{ stay.reviews.length }})
+        </span>
       </p>
-    </h1>
-    <main>
-      <p>{{ stay.loc.city }}, {{ stay.loc.country }}</p>
-      <p>{{ stay.capacity }} bads</p>
-    </main>
-    <h1 class="cost"><span class="price">${{ stay.price }}</span> night</h1>
+      <main>
+        <p>{{ stay.loc.city }}, {{ stay.loc.country }}</p>
+        <p>{{ stay.capacity }} bads</p>
+      </main>
+      <p class="cost"><span class="price">${{ stay.price }}</span> night</p>
+    </section>
   </section>
 </template>
 <script>
