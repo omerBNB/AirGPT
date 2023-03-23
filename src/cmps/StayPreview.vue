6f931efacfb96978da00176791dc913bb62f1d38
Inon <inon.f4@gmail.com>
2023-03-23 18:20:02 +0200

    preview cards of stays

--- src/cmps/StayPreview.vue
+++ src/cmps/StayPreview.vue
@@ -3,7 +3,21 @@
     <StayImgPreview :imgs="stay.imgUrls" />
     <h1 class="stay-name flex">
       {{ stay.name }}
-      <p class="rate flex">{{ stay.reviews[0].rate }} ({{ stay.reviews.length }}) (stars)</p>
+      <p class="rate flex">
+        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation"
+          focusable="false" style="
+                          display: block;
+                          height: 14px;
+                          width: 14px;
+                          margin-bottom: 1px;
+                          fill: currentcolor;
+                        ">
+          <path
+            d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z"
+            fill-rule="evenodd"></path>
+        </svg>
+        {{ stay.reviews[0].rate }} ({{ stay.reviews.length }})
+      </p>
     </h1>
     <main>
       <p>{{ stay.loc.city }}, {{ stay.loc.country }}</p>
@@ -12,7 +26,6 @@
     <h1 class="cost"><span class="price">${{ stay.price }}</span> night</h1>
   </section>
 </template>
-
 <script>
 import StayImgPreview from '../cmps/StayImgPreview.vue'
 export default {
