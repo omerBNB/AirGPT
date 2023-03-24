aab6f835e2c42c03766c6b2b00a3144b0ea12fdd
Omer <omerbnb@gmail.com>
2023-03-24 16:40:04 +0300

    layout

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import './assets/styles/setup/_typography.scss'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import VCalendar from 'v-calendar';
+import 'v-calendar/style.css';
+
+// Use plugin with optional defaults
+
+
 import './assets/styles/main.scss'
 import App from './App.vue'
 
@@ -15,4 +21,5 @@ const app = createApp(App)
 app.use(router)
 app.use(store)
 app.use(ElementPlus)
+app.use(VCalendar, {})
 app.mount('#app')
