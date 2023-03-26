3b5636f17048b1fb3ba6d3a74bb1edd51643b4dc
Inon <inon.f4@gmail.com>
2023-03-26 19:36:29 +0300

    preview

--- src/cmps/StayPreview.vue
+++ src/cmps/StayPreview.vue
@@ -1,18 +1,19 @@
 <template>
   <section class="stay-card">
-    <StayImgPreview @details="details()" @updateWishList="$emit('updateWishList')" :imgs="stay.imgUrls" />
+    <StayImgPreview @details="details()" :imgs="stay.imgUrls" />
+    <hart :class="{ mark: isMark }" />
     <section class="info" @click="details()">
       <p class="stay-name flex">
         {{ stay.name }}
         <span class="rate flex">
           <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation"
             focusable="false" style="
-                                                        display: block;
-                                                        height: 14px;
-                                                        width: 14px;
-                                                        margin-bottom: 1px;
-                                                        fill: currentcolor;
-                                                      ">
+                                                                                                          display: block;
+                                                                                                          height: 14px;
+                                                                                                          width: 14px;
+                                                                                                          margin-bottom: 1px;
+                                                                                                          fill: currentcolor;
+                                                                                                        ">
             <path
               d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z"
               fill-rule="evenodd"></path>
@@ -32,10 +33,19 @@
 </template>
 <script>
 import StayImgPreview from '../cmps/StayImgPreview.vue'
+import hart from '../imgs/svg_symbols/hart.vue'
 export default {
   name: 'StayPreview',
   props: {
     stay: Object,
+  }, 
+  created() {
+    // inWishList?
+  },
+  data() {
+    return {
+      isMark: true
+    }
   },
   methods: {
     details() {
@@ -53,6 +63,6 @@ export default {
     },
   },
   emits: ['updateWishList'],
-  components: { StayImgPreview },
+  components: { StayImgPreview, hart },
 }
 </script>
