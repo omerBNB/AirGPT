8f6e13a8cd7497ee72a476675b6009ae8a7cd7fd
Inon <inon.f4@gmail.com>
2023-04-02 09:06:33 +0300

    pll

--- src/cmps/DetailsReviews.vue
+++ src/cmps/DetailsReviews.vue
@@ -2,20 +2,14 @@
   <main v-if="currStay" class="reviews-container">
     <section class="reviews-header">
       <section class="reviews-rate flex">
-        <span
-          ><svg
-            viewBox="0 0 32 32"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            role="presentation"
-            focusable="false"
-            style="
-              display: block;
-              height: 14px;
-              width: 14px;
-              margin-bottom: 1px;
-              fill: currentcolor;
-            ">
+        <span><svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation"
+            focusable="false" style="
+                  display: block;
+                  height: 14px;
+                  width: 14px;
+                  margin-bottom: 1px;
+                  fill: currentcolor;
+                ">
             <path
               d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z"
               fill-rule="evenodd"></path>
@@ -23,9 +17,7 @@
         </span>
         <span class="bold-font">{{ currStay.rate }}</span>
         <span> • </span>
-        <span
-          ><a class="bold-font" href="">{{ reviewsNum }}</a></span
-        >
+        <span><a class="bold-font" href="">{{ reviewsNum }}</a></span>
       </section>
 
       <section class="rating grid">
@@ -69,15 +61,13 @@
     <section>
       <!-- Guest Reviews -->
       <section class="guest-reviews-container">
-        <article v-for="review in currStay.reviews" class="review flex">
+        <article v-for="review in this.currStay.reviews" class="review flex">
           <div class="mini-user-details flex">
             <img :src="review.by.imgUrl" />
             <div>
               <h4>{{ review.by.fullname }}</h4>
               <span>{{
-                new Intl.DateTimeFormat('en-US', { month: 'long', year: 'numeric' }).format(
-                  new Date(review.at)
-                )
+                new Intl.DateTimeFormat('en-US', { month: 'long', year: 'numeric' }).format(new Date(review.at))
               }}</span>
             </div>
           </div>
@@ -113,7 +103,7 @@ export default {
       return this.stay.reviews.length + ' reviews'
     },
 
-    formatReviewDate(review) {},
+    formatReviewDate(review) { },
   },
   created() {
     this.currStay = this.stay
