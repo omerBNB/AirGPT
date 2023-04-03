153e7979d49908c29087a5740b2d29f6d3eda058
Omer <omerbnb@gmail.com>
2023-04-03 18:53:44 +0300

    svg

--- src/views/StayDetailsDesktop.vue
+++ src/views/StayDetailsDesktop.vue
@@ -61,8 +61,8 @@
             <h3>What this place offers</h3>
             <div class="offers grid">
               <div v-for="amenitie in this.stay.amenities" class="offer">
-                <!-- <img :src="'../../src/imgs/amenities/' + amenitie + '.svg'" /> -->
-                <img :src="'../../src/imgs/amenities/tv.svg'" />
+                <img :src="'../src/imgs/amenities/' + amenitie + '.svg'" />
+                <!-- <img :src="'../imgs/svg_symbols/tv.svg'" /> -->
                 <p>{{ amenitie }}</p>
               </div>
               <p
