4410106be173036d6c110968dc48488688c2329c
Inon <inon.f4@gmail.com>
2023-04-04 11:42:29 +0300

    trips

--- src/cmps/NextStayPreview.vue
+++ src/cmps/NextStayPreview.vue
@@ -1,11 +1,12 @@
 <template>
   <section class="flex">
     <section>
-      <h1 class="main-header">Your next stay</h1>
+      <h1 class="main-header">Your next stay <i class="fa-solid fa-circle-check"></i></h1>
       <section class="info flex flex-column">
         <h1 class="header">{{ nextStay.nextStay.name }}</h1>
         <h1>{{ nextStay.nextStay.loc.city }}, {{ nextStay.nextStay.loc.country }}</h1>
-        <h1 class="dates">{{ nextStay.checkin }} - {{ nextStay.checkout }}</h1>
+        <h1 class="dates">05/05/23 - 26/05/23</h1>
+        <!-- <h1 class="dates">{{ nextStay.checkin }} - {{ nextStay.checkout }}</h1> -->
       </section>
     </section>
     <img :src="nextStay.nextStay.imgUrls[0]" />
--- src/cmps/PrevStaysPreview.vue
+++ src/cmps/PrevStaysPreview.vue
@@ -4,14 +4,27 @@
         <section class="info">
             <h1 class="header"> {{ stayToPreview.loc.city }}, {{ stayToPreview.loc.country }}</h1>
             <h1>{{ stayToPreview.name }}</h1>
+            <h1 class="dates">{{ getDates }}</h1>
             <!-- <h1 class="dates">{{ nextStay.checkin }} - {{ nextStay.checkout }}</h1> -->
         </section>
     </section>
 </template>
 <script>
+import { utilService } from '../services/util.service'
 export default {
     props: {
         stayToPreview: Object,
+    },
+    computed: {
+        getDates() {
+            return `
+            ${utilService.getRandomIntInclusive(1, 30)}
+            /${utilService.getRandomIntInclusive(1, 6)}
+            /23 - ${utilService.getRandomIntInclusive(1, 30)}
+            /${utilService.getRandomIntInclusive(6, 12)}
+            /23
+            `
+        }
     },
 }
 </script>
